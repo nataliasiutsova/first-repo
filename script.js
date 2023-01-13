92d5308cc890e7fcf8948452375920d4aff905e0
nataliasiutsova <tashaklimenkova@gmail.com>
2023-01-13 23:43:43 +0300

    Added script

---
+++ script.js
@@ -0,0 +1,16 @@
+let a = 1;
+let b = 34;
+console.log(a + b);
+function sum(c, f) {
+  let v = c + f;
+  return v;
+}
+console.log(sum(3, 5));
+
+// ДЗ 1.4
+
+const sayHelloLinting = (fName) => {
+  console.log(`Look, how pretty it is, ${fName}`);
+};
+
+sayHelloLinting('Oksana');
